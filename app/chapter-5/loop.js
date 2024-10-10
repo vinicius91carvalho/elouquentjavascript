0ac73033d9a273d30d0d59ff3087bfe116d40ab2
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2024-10-10 11:43:25 -0300

    feat(chapter-5): loop

---
+++ app/chapter-5/loop.js
@@ -0,0 +1,25 @@
+/**
+ * Your own loop 
+ * Write a higher-order function loop that provides something like a for loop 
+ * statement. It should take a value, a test function, an update function, and 
+ * a body function. Each iteration, it should first run the test function on the
+ * current loop value and stop if that returns false. It should then call the 
+ * body function, giving it the current value, and finally call the update 
+ * function to create a new value and start over from the beginning.
+ * 
+ * When defining the function, you can use a regular loop to do the actual 
+ * looping.
+ */
+
+function loop(value, testFunc, updatedFunc, bodyFunc) {
+    if (testFunc(value)) {
+        bodyFunc(value);
+        const newValue = updatedFunc(value);
+        loop(newValue, testFunc, updatedFunc, bodyFunc);
+    }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
